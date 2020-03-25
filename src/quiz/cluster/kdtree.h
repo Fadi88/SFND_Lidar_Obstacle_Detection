0ab8fa4f68d1024b6c1e7d7bbb397b1b6b96a90f
Fadi Sedik <fady88@gmail.com>
2020-03-25 10:07:00 +0100

    adding KD tree search custom imp for 2d

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -25,16 +25,47 @@ struct KdTree
 	: root(NULL)
 	{}
 private:
-	void insert_reccursive(Node** parent,std::vector<float> pt,int id,int depth){
+	void insert_recursive(Node** parent,std::vector<float> pt,int id,int depth){
 		if(*parent==NULL)
 			*parent = new Node(pt,id);
 		else{
 			if(pt[depth%2] < (*parent)->point[depth%2])
-				insert_reccursive(&((*parent)->left) , pt , id , depth +1 );
+				insert_recursive(&((*parent)->left) , pt , id , depth +1 );
 			else
-				insert_reccursive(&((*parent)->right) , pt , id , depth +1 );
+				insert_recursive(&((*parent)->right) , pt , id , depth +1 );
 		}
 		
+	}
+
+	void search_recursive(const std::vector<float>& target, float distance_tolerance, int depth, Node* parent,std::vector<int> &ids){
+		if(parent != NULL){
+			std::cout << "checking node : " << parent->id << std::endl;
+
+			if(
+				(parent->point[0] >= target[0]-distance_tolerance) &&
+				(parent->point[0] <= target[0]+distance_tolerance) &&
+				(parent->point[1] >= target[1]-distance_tolerance) &&
+				(parent->point[1] <= target[1]+distance_tolerance)
+			){
+				float distance = std::sqrt(
+					((parent->point[0] - target[0]) * (parent->point[0] - target[0]))
+					+
+					((parent->point[1] - target[1]) * (parent->point[1] - target[1]))
+				);
+
+				if(distance <= distance_tolerance){
+					ids.push_back(parent->id);
+				}
+			}
+
+			if((target[depth%2] - distance_tolerance) < parent->point[depth%2])
+				search_recursive(target , distance_tolerance , depth + 1 , parent->left , ids );
+
+			if((target[depth%2] + distance_tolerance) > parent->point[depth%2])
+				search_recursive(target , distance_tolerance , depth + 1 , parent->right , ids );
+
+
+		}
 	}
 
 public:
@@ -42,7 +73,7 @@ public:
 	{
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root
-		insert_reccursive(&root , point , id , 0);
+		insert_recursive(&root , point , id , 0);
 
 	}
 
@@ -50,6 +81,7 @@ public:
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		search_recursive(target,distanceTol,0,root,ids);
 		return ids;
 	}
 	
